687761f222255624317fb4c562a0883f71b52968
Fengda HUANG <h@phodal.com>
2015-08-14 22:34:21 +0800

    add flash id

--- bin/esptoolpy.js
+++ bin/esptoolpy.js
@@ -9,11 +9,19 @@ var ESP = require('../esptoolpy');
 
 program
   .version('0.0.1')
-  .option('-m, --read_mac [mode]', 'Read Mac Info')
+  .option('--read_mac [mode]', 'Read Mac Info')
+  .option('--flash_id [mode]', 'Read Flash ID')
   .parse(process.argv);
 
 if (program.read_mac) {
   ESP.read_mac(program.read_mac, function(result){
+    console.log(result.toString());
+  });
+  //console.log(result);
+}
+
+if (program.flash_id) {
+  ESP.flash_id(program.flash_id, function(result){
     console.log(result.toString());
   });
   //console.log(result);
--- esptoolpy.js
+++ esptoolpy.js
@@ -2,7 +2,6 @@ var spawn = require('child_process').spawn;
 
 
 function read_mac (port, cb){
-  var result = '';
   var command = spawn('python', ['lib/esptool.py', '-p', port, 'read_mac']);
 
   command.stdout.on('data', function (data) {
@@ -18,6 +17,23 @@ function read_mac (port, cb){
   });
 }
 
+function flash_id (port, cb){
+  var command = spawn('python', ['lib/esptool.py', '-p', port, 'flash_id']);
+
+  command.stdout.on('data', function (data) {
+    'use strict';
+    //console.log('stdout: ' + data);
+    cb(data);
+  });
+
+  command.stderr.on('data', function (data) {
+    'use strict';
+    //console.log('stderr: ' + data);
+    cb(data);
+  });
+}
+
 module.exports = {
-  read_mac :read_mac
+  read_mac :read_mac,
+  flash_id :flash_id
 };
